d198cb658d5fcb900226131c23b9cdbe42dd9565
Kaiswim1 <68935355+Kaiswim1@users.noreply.github.com>
2024-12-18 00:36:26 -0500

    Update GameJoiner.java

--- GameJoiner.java
+++ GameJoiner.java
@@ -39,20 +39,16 @@ public class GameJoiner {
         return null;
     }
 
-
-
-
-    // Method to send data (POST request)
-    public static void sendData(int score) throws IOException {
-
-        URL url = new URL(SERVER_URL);
+    public static void sendData(String key, int value) throws IOException {
+        URL url = new URL(SERVER_URL + "data");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setDoOutput(true);
 
         // JSON data to send
-        String jsonInputString = "{\"score\": " + score + "}";
+        String jsonInputString = String.format("{\"key\":\"%s\",\"value\":%d}", key, value);
+
 
         // Send the data
         try (OutputStream os = connection.getOutputStream()) {
@@ -64,16 +60,33 @@ public class GameJoiner {
         int responseCode = connection.getResponseCode();
         System.out.println("Response Code: " + responseCode);
 
-        if (responseCode == HttpURLConnection.HTTP_OK) { // Success
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-                String responseLine;
-                while ((responseLine = br.readLine()) != null) {
-                    System.out.println(responseLine);
-                }
-            }
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            System.out.println("Value updated successfully.");
         } else {
             System.out.println("Error: " + connection.getResponseMessage());
         }
+    }
+
+    // Method to get a specific variable by key (GET request)
+    // Method to get a specific variable by key (GET request)
+    // Method to get a specific variable by key (GET request)
+    public static int getData(String key) throws IOException {
+        // Fix the URL to match the server route (/data/:key)
+        URL url = new URL(SERVER_URL + "data/" + key);  // Use "data/" before the key
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int data = 0;
+        // Get the response
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+            String responseLine;
+            while ((responseLine = br.readLine()) != null) {
+                String r = responseLine.split(":")[1];
+                data = Integer.parseInt(r.substring(0, r.length()-1));
+                System.out.println("Response from server: " + data);
+            }
+        }
+        return data;
     }
 
     private static String getFirstThreeOctets(String ipAddress) {
@@ -90,32 +103,12 @@ public class GameJoiner {
         return "";
     }
 
-    // Method to get data (GET request)
-    public static void getData() throws IOException {
-        URL url = new URL(SERVER_URL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-
-        // Get the response
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-            String responseLine;
-            while ((responseLine = br.readLine()) != null) {
-                System.out.println("Response from server: " + responseLine);
-            }
-        }
-    }
-
-    public static void connect(String host) {
-        // Dynamically retrieve the local IP address and construct the server URL
+    public static int connect(String host) throws IOException {
         System.out.println(getFirstThreeOctets(getLocalIPAddress()));
-        SERVER_URL = "http://" + getFirstThreeOctets(getLocalIPAddress()) + host + ":3000/data"; // Using the local IP address for server communication
+        SERVER_URL = "http://" + getFirstThreeOctets(getLocalIPAddress()) + host + ":3000/"; // Using the local IP address for server communication
+        int pNum = getData("manyPlayers")+1;
+        sendData("manyPlayers", pNum);
+        return pNum;
 
-        try {
-            // Example of sending and receiving data
-            sendData(290); // Uncomment to send a new score
-            getData();    // Retrieve the current score
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
