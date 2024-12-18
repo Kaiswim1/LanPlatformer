ffb78c454f6c37bce5a8ffa924433c72befcb2b9
Kaiswim1 <68935355+Kaiswim1@users.noreply.github.com>
2024-12-18 00:35:41 -0500

    Update GameHoster.java

--- GameHoster.java
+++ GameHoster.java
@@ -35,16 +35,17 @@ public class GameHoster {
         return null;
     }
 
-    // Method to send data (POST request)
-    public static void sendData(int score) throws IOException {
-        URL url = new URL(SERVER_URL);
+    // Method to send a specific variable (POST request)
+    public static void sendData(String key, int value) throws IOException {
+        URL url = new URL(SERVER_URL + "data");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setDoOutput(true);
 
         // JSON data to send
-        String jsonInputString = "{\"score\": " + score + "}";
+        String jsonInputString = String.format("{\"key\":\"%s\",\"value\":%d}", key, value);
+
 
         // Send the data
         try (OutputStream os = connection.getOutputStream()) {
@@ -56,32 +57,32 @@ public class GameHoster {
         int responseCode = connection.getResponseCode();
         System.out.println("Response Code: " + responseCode);
 
-        if (responseCode == HttpURLConnection.HTTP_OK) { // Success
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-                String responseLine;
-                while ((responseLine = br.readLine()) != null) {
-                    System.out.println(responseLine);
-                }
-            }
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            System.out.println("Value updated successfully.");
         } else {
             System.out.println("Error: " + connection.getResponseMessage());
         }
     }
-
-    // Method to get data (GET request)
-    public static void getData() throws IOException {
-        URL url = new URL(SERVER_URL);
+    
+    public static int getData(String key) throws IOException {
+        // Fix the URL to match the server route (/data/:key)
+        URL url = new URL(SERVER_URL + "data/" + key);  // Use "data/" before the key
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
 
+        int data = 0;
         // Get the response
         try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
             String responseLine;
             while ((responseLine = br.readLine()) != null) {
-                System.out.println("Response from server: " + responseLine);
+                String r = responseLine.split(":")[1];
+                data = Integer.parseInt(r.substring(0, r.length()-1));
+                System.out.println("Response from server: " + data);
             }
         }
+        return data;
     }
+
 
     public static String getLastOctet(String ipAddress) {
         // Regex pattern to match the last octet of an IP address
@@ -97,22 +98,13 @@ public class GameHoster {
         return "";
     }
 
-    public static void connect(){
-        // Dynamically retrieve the local IP address and construct the server URL
+
+
+    // Method to connect to the server and demonstrate sending/retrieving data
+    public static int connect() throws IOException {
         String localIP = getLocalIPAddress();
-        if (localIP != null) {
-            SERVER_URL = "http://" + localIP + ":3000/data"; // Using the local IP address for server communication
-            System.out.println("Connecting to server at: " + SERVER_URL);
-            Thread thread = new Thread(() -> {
-                /*sendData(280);
-                getData();*/
-            });
-            thread.start();
-
-        } else {
-            System.out.println("Local IP not found.");
-        }
+        SERVER_URL = "http://" + localIP + ":3000/";
+        sendData("manyPlayers", 1); //First player to connect
+        return 1;
     }
-
-
 }
